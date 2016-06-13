73069bcb5c5497306132213a7c8ef59105e726bd
Caleb James Smith <colorado.caleb.smith@gmail.com>
2016-06-13 11:27:29 -0500

    Come along with me...

--- client/bridge_test.py
+++ client/bridge_test.py
@@ -28,9 +28,9 @@ def runBridgeTests(RMList, num_slots, num_tests, verbosity=0):
             print '\n-------------------- Test Slot: ', slot, ' --------------------'
             test_list = basicTests(slot,num_tests)
             total_test_list = map(add, total_test_list, test_list)
-            daisyChain = q.qCard(webBus("pi5",0), q.QIEi2c[slot])
-            print '\n~~~~~~~~~~ QIE Daisy Chain ~~~~~~~~~~'
-            print str(daisyChain)
+            # daisyChain = q.qCard(webBus("pi5",0), q.QIEi2c[slot])
+            # print '\n~~~~~~~~~~ QIE Daisy Chain ~~~~~~~~~~'
+            # print str(daisyChain)
             if verbosity:
                 print '\nNumber passed = ', test_list[0]
                 print 'Number failed = ', test_list[1]
@@ -417,4 +417,4 @@ i2cDict = {
 ###############################################################################
 
 # runBridgeTests(RMList, num_slots, num_tests, verbosity=0)
-runBridgeTests([0],1,0,0)
+runBridgeTests([0],4,27,0)
