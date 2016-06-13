8273718acb72b702792913095c40a1c8544c5718
Caleb James Smith <colorado.caleb.smith@gmail.com>
2016-06-13 11:20:06 -0500

    Come along with me...

--- client/bridge_test.py
+++ client/bridge_test.py
@@ -29,7 +29,7 @@ def runBridgeTests(RMList, num_slots, num_tests, verbosity=0):
             test_list = basicTests(slot,num_tests)
             total_test_list = map(add, total_test_list, test_list)
             daisyChain = q.qCard(webBus("pi5",0), q.QIEi2c[slot])
-            print q.QIEi2c[slot]
+            print '\n~~~~~~~~~~ QIE Daisy Chain ~~~~~~~~~~'
             print str(daisyChain)
             if verbosity:
                 print '\nNumber passed = ', test_list[0]
