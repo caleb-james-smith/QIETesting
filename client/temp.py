28441d9e0cd1e64f8f1ed6261cefd5d680152cdd
Caleb James Smith <colorado.caleb.smith@gmail.com>
2016-06-17 16:54:50 -0500

    Come along with me...

--- client/temp.py
+++ client/temp.py
@@ -115,4 +115,4 @@ def run(rmList,slotList,iterations,verbosity=0):
                 print '\n-----\n',key, ' ', hold,'\n-----\n'
                 readManyTemps(slot,iterations,key,hold,verbosity)
 
-run([0],[0,1,2,3],200,0)
+run([0],[0],100,0)
