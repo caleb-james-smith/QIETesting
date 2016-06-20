b7fa25c8d8efb45bc9a98e885bfc3cc4ddb1e32b
Caleb James Smith <colorado.caleb.smith@gmail.com>
2016-06-20 16:41:01 -0500

    Come along with me...

--- client/newFanout.py
+++ client/newFanout.py
@@ -1,7 +1,7 @@
 # new fanout pi6
 
 from client import webBus
-b = webBus("pi5",0)
+b = webBus("pi6",0)
 bus5 = webBus("pi5",0)
 bus6 = webBus("pi6",0)
 
@@ -15,48 +15,48 @@ def ngccmGroup(rm):
     i2cGroups = [0x01,0x10,0x20,0x02]
     return i2cGroups[rm-1]
 
-def openRM(rm):
+def openRM(rm,bus):
     # b.write(0x00,[0x06])
     if rm in [1,2]:
         # Open channel to ngCCM for RM 0,1: J1 - J10
         print '### Open RM ', rm
-        b.write(0x72,[1])
+        bus.write(0x72,[1])
     elif rm in [3,4]:
         # Open channel to ngCCM for RM 2,3: J17 - J26
         print '### Open RM ', rm
-        b.write(0x72,[2])
+        bus.write(0x72,[2])
     else:
         print 'Invalid RM = ', rm
         print 'Please choose RM = {0,1,2,3}'
         return 'closed channel'
     # Open channel to i2c group
-    b.write(0x74, [ngccmGroup(rm)])
+    bus.write(0x74, [ngccmGroup(rm)])
     # b.read(0x74,1)
     # b.write(0x74, [0xFF])
-    return b.sendBatch()
+    return bus.sendBatch()
 
-def bridgeRead(cardList,nBytes):
+def bridgeRead(cardList,nBytes,bus):
     for card in cardList:
-        b.write(bridgeAddress(card),[0x0])
-        b.read(bridgeAddress(card),nBytes)
-    return b.sendBatch()
+        bus.write(bridgeAddress(card),[0x0])
+        bus.read(bridgeAddress(card),nBytes)
+    return bus.sendBatch()
 
 def findRM(rmList):
     for rm in rmList:
         print openRM(rm)
         print bridgeRead([2,4],1)
 
-def search(nGroups):
-    b.write(0x72,[0x01])
-    b.read(0x72,1)
+def search(nGroups,bus):
+    bus.write(0x72,[0x01])
+    bus.read(0x72,1)
     print '0x72 = ',b.sendBatch()
     for i in xrange(nGroups):
         byte = 2**i
         print '\n--- Write to 0x74: ',hex(byte),' = ',byte
-        b.write(0x74,[byte])
-        b.read(0x74,1)
-        print '0x74 = ',b.sendBatch()
-        print 'Bridge Read = ',bridgeRead([1,2,3,4],4)
+        bus.write(0x74,[byte])
+        bus.read(0x74,1)
+        print '0x74 = ',bus.sendBatch()
+        print 'Bridge Read = ',bridgeRead([1,2,3,4],4,bus)
 
 def write70(bus):
     bus.write(0x72,[0x01])
@@ -71,13 +71,14 @@ def write70(bus):
     bus.write(0x70,[1,0])
     print bus.sendBatch()
 
-def readClock(add,bus):
-    openRM(1)
+def readClock(rm,slot,bus):
+    openRM(rm,bus)
     for i in xrange(4):
         bus.write(add,[0x12])
         bus.read(add,4)
-    bus.sendBatch
+        print bus.sendBatch
 
 # write70(bus6)
-# search(1)
-openRM(1)
+# openRM(1)
+# search(8,bus6)
+readClock(1,4,bus6)
