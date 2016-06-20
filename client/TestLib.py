1bf1d58e73b5a4507e8cf2b410a27503c5a0d9da
Caleb James Smith <colorado.caleb.smith@gmail.com>
2016-06-20 14:53:52 -0500

    Come along with me...

--- client/TestLib.py
+++ client/TestLib.py
@@ -144,21 +144,15 @@ def readRegisterIgloo(slot, address, num_bytes):
 def openRM(rm):
     if rm in [0,1]:
         # Open channel to ngCCM for RM 0,1: J1 - J10
-        # print '##### RM ', rm
         b.write(q.MUXs["fanout"],[0x02])
-        # b.sendBatch()
     elif rm in [2,3]:
         # Open channel to ngCCM for RM 2,3: J17 - J26
-        # print '##### RM ', rm
         b.write(q.MUXs["fanout"],[0x01])
-        # b.sendBatch()
     else:
         print 'Invalid RM = ', rm
         print 'Please choose RM = {0,1,2,3}'
         return 'closed channel'
     # Open channel to i2c group
-    # print '##### open i2c ' + hex(q.RMi2c[rm])
-    # b.clearBus()
     b.write(q.MUXs["ngccm"]["u10"], [q.RMi2c[rm]])
     return b.sendBatch()
 
--- client/newFanout.py
+++ client/newFanout.py
@@ -14,19 +14,15 @@ def open(rm):
         # Open channel to ngCCM for RM 0,1: J1 - J10
         print '### Open RM ', rm
         b.write(0x72,[0x02])
-        # b.sendBatch()
     elif rm in [2,3]:
         # Open channel to ngCCM for RM 2,3: J17 - J26
         print '### Open RM ', rm
         b.write(0x72,[0x01])
-        # b.sendBatch()
     else:
         print 'Invalid RM = ', rm
         print 'Please choose RM = {0,1,2,3}'
         return 'closed channel'
     # Open channel to i2c group
-    # print '##### open i2c ' + hex(q.RMi2c[rm])
-    # b.clearBus()
     b.write(0x74, [ngccmGroup(rm)])
     return b.sendBatch()
 
